01aa397847f8cae41d4b84d8fdf1dae406f06408
kelsey-schenk <kelsey.schenk@att.net>
2022-01-25 17:13:29 -0500

    preliminiary movies model

--- models/reviews.js
+++ models/reviews.js
@@ -1,3 +1,37 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+class reviews extends Model {}
+
+reviews.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        review_text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        movies_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'movies',
+                key: 'id'
+            }
+        }
+        // rating provided??
+    }
+)
