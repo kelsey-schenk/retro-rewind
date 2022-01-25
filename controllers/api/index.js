7a49cf5fbcc562739277217a72594d7d8cca058b
kelsey-schenk <kelsey.schenk@att.net>
2022-01-24 20:42:24 -0500

    got server started :)

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,13 +3,9 @@ const reviewsRoutes = require('./reviews-routes');
 const userRoutes = require('./user-routes');
 const filmsRoutes = require('./films-routes');
 
+
 router.use('/reviews', reviewsRoutes);
 router.use('/products', userRoutes);
 router.use('/films', filmsRoutes);
-
-router.use('/api', apiRoutes);
-router.use((req, res) => {
-    res.status(404).end();
-});
 
 module.exports = router;
