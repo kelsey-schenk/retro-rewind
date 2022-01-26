75d113dd994841f4f3cdabb30f266da8c4d6ecd7
sboucher <sboucher2011@gmail.com>
2022-01-25 19:20:53 -0500

    initial bulma setup and working with handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,4 +4,16 @@ router.get('/', (req, res) => {
   res.render('homepage');
 });
 
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+router.get('/dashboard', (req, res) => {
+  res.render('dashboard');
+});
+
 module.exports = router;
