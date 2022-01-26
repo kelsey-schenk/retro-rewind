878135f2bb90350060db6d10e725db550843b029
kelsey-schenk <kelsey.schenk@att.net>
2022-01-25 20:15:49 -0500

    rentals model changes

--- models/rentals.js
+++ models/rentals.js
@@ -3,4 +3,28 @@ const sequelize = require('../config/connection');
 
 class rentals extends Model {}
 
-rentals.init()
+rentals.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            }
+        },
+        movie_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'movies',
+                key: 'id'
+            }
+        }
+    }
+
+)
