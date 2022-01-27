bb6ea73634f24139a544c0b04cd63df360bd810b
kelsey-schenk <kelsey.schenk@att.net>
2022-01-26 20:35:49 -0500

    movies routes

--- controllers/api/films-routes.js
+++ controllers/api/films-routes.js
@@ -20,30 +20,54 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-    // find a single product by its `id`
-    // be sure to include its associated Category and Tag data
-    Product.findOne(
+    Movies.findOne(
     {
         where: {
         id: req.params.id
         },
         include: [
         {
-            model: Category,
-            attributes: ['category_name']
+            model:  Movies,
+            attributes: ['id', 'user_id', 'title', 'rating', 'status']
         },
-        {
-            model: Tag,
-            attributes: ['tag_name']
-        }
         ]
     }
     )
-    .then(productData => res.json(productData))
-      .catch(err => {
+    .then(moviesData => res.json(moviesData))
+    .catch(err => {
         console.log(err);
         res.status(500).json(err)
-      });
-  });
+    });
+});
+
+router.post('/', (req, res) => {
+    /* req.body should look like this...
+    {
+        product_name: "Basketball",
+        price: 200.00,
+        stock: 3,
+        tagIds: [1, 2, 3, 4]
+    }
+    */
+    Movies.create(req.body)
+    .then((product) => {
+        if (req.body.tagIds.length) {
+        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+            return {
+            product_id: product.id,
+            tag_id,
+            };
+        });
+        return ProductTag.bulkCreate(productTagIdArr);
+        }
+        res.status(200).json(product);
+    })
+    .then((productTagIds) => res.status(200).json(productTagIds))
+    .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+});
+
 
 module.exports = router;
