07fa91c241059374a9c1dac4d94cbfee4e74b033
Erik Jones <jonzee1914@gmail.com>
2022-01-27 15:09:21 -0500

    move signup button into form still doesn't work

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
+
 const reviewsRoutes = require('./reviews-routes');
 const userRoutes = require('./user-routes');
 const filmsRoutes = require('./films-routes');
 
 
 router.use('/reviews', reviewsRoutes);
-router.use('/products', userRoutes);
+router.use('/users', userRoutes);
 router.use('/films', filmsRoutes);
 
 module.exports = router;
