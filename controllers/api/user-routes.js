e3f20ee6a056c89484b31c5bb0dda076e5ce9ca6
kelsey-schenk <kelsey.schenk@att.net>
2022-01-26 19:58:40 -0500

    user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
-const { User, Review, Rental, Movie } = require('../../models');
-const rentals = require('../../models/rentals');
-const reviews = require('../../models/reviews');
+const { User, Review, Rentals, Movie } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -24,22 +22,99 @@ router.get('/:id', (req, res) => {
         },
         include: [
             {
-                model: movies,
+                model: Movie,
                 attributes: ['id', 'movie_id', 'title']
             },
             {
-                model: rentals,
+                model: Rentals,
                 attributes: ['id', 'movie_id'],
-                through: rentals,
+                through: Rentals,
                 as: 'rented_movies'
             },
             {
-                model: reviews,
-                attributes: []
+                model: Review,
+                attributes: ['id', 'review_title'],
+                through: Review,
+                as: 'reviews_written'
             }
         ]
     })
     .then(dbUserData =>{
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// POST /api/users
+router.post('/', (req,res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+// Login route
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' })
+            return;
+        }
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+});
+
+// PUT /api/users/1
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(404).json({ message: 'No user found with this id'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// DELETE /api/users/1
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
         if (!dbUserData) {
             res.status(404).json({ message: 'No user found with this id' });
             return;
