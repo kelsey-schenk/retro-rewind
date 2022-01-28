a3ad0b0196d451c4e8c658336f178e878af4a566
sboucher <sboucher2011@gmail.com>
2022-01-28 10:26:03 -0500

    added movie modal

---
+++ public/javascript/addMovie.js
@@ -0,0 +1,64 @@
+//-----------------------------------
+// Modal Logic
+//-----------------------------------
+// Variables
+var modal = document.getElementById("add-movie-modal");
+var btn = document.getElementById("addMovie");
+
+// Close Modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+//-----------------------------------
+// Add Review Logic
+//-----------------------------------
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+const rating = document.querySelector('#rating-selection').value.trim();
+const title = document.querySelector('#review-title').value.trim();
+const description = document.querySelector('#review-description').value.trim();
+
+console.log(rating);
+console.log(title);
+console.log(description);
+
+/*
+if (email && password) {
+  const response = await fetch('/api/users/login', {
+    method: 'post',
+    body: JSON.stringify({
+      email,
+      password
+    }),
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+}
+*/
+
+}
+
+
+document.querySelector('.movie-form').addEventListener('submit', loginFormHandler);
