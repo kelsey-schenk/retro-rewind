b0bd5cf35d59f3231f5db972a913769b9a347bfa
kelsey-schenk <kelsey.schenk@att.net>
2022-01-27 19:10:25 -0500

    changes to movies routes

--- controllers/api/films-routes.js
+++ controllers/api/films-routes.js
@@ -43,26 +43,15 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
     /* req.body should look like this...
     {
-        product_name: "Basketball",
-        price: 200.00,
-        stock: 3,
-        tagIds: [1, 2, 3, 4]
+        title: "Free Willy",
+        rating: "PG",
+        description: "Movie Description",
     }
     */
     Movies.create(req.body)
-    .then((product) => {
-        if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-            return {
-            product_id: product.id,
-            tag_id,
-            };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-        }
-        res.status(200).json(product);
+    .then((movie) => {
+        res.status(200).json(movie;
     })
-    .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
         console.log(err);
         res.status(400).json(err);
