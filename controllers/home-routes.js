0721fffb140eb737a93594a9acab4adaac2562a6
sboucher <sboucher2011@gmail.com>
2022-01-29 09:46:58 -0500

    working get movies function on searchMovies page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { User, Reviews, Rentals, Movie } = require('../models');
+const { User, Reviews, Rentals, Movie, Movies } = require('../models');
 
 //Index Links
+
 router.get('/', (req, res) => {
   console.log(req.session);
   res.render('homepage', {
     loggedIn: req.session.loggedIn
   });
 });
+
 
 router.get('/login', (req, res) => {
   res.render('login');
@@ -28,9 +30,14 @@ router.get('/movie', (req, res) => {
   res.render('movie');
 });
 
+/*
 router.get('/searchMovies', (req, res) => {
-  res.render('searchMovies');
+  res.render('searchMovies', {
+    title: 'Handlebars Docs',
+    description: 'movie description'
+  });
 });
+*/
 
 //USER SESSION
 router.get('/login', (req, res) => {
@@ -42,12 +49,23 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-
-router.get('/', (req, res) => {
-  res.render('searchMovies', {
-    title: 'Handlebars Docs',
-    description: 'movie description'
-  });
+router.get('/searchMovies', (req, res) => {
+  Movies.findAll({
+    attributes: ['id', 'title', 'rating', 'description', 'status', 'user_id'],
+    
+  })
+    .then(dbPostData => {
+      // pass a single post object into the homepage template
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      console.log(posts[0])
+      res.render('searchMovies', {
+        posts
+        //loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
-
 module.exports = router;
