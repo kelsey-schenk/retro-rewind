2a53050d46b54a7f9fefe14c52bb6e1e386a23b3
sboucher <sboucher2011@gmail.com>
2022-01-29 11:20:45 -0500

    added user movies to dashboard

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -19,11 +19,13 @@ router.get('/signup', (req, res) => {
   res.render('signup');
 });
 
+/*
 router.get('/dashboard', (req, res) => {
   res.render('dashboard', {
     loggedIn: req.session.loggedIn
   });
 });
+*/
 
 router.get('/movie', (req, res) => {
   res.render('movie');
@@ -48,8 +50,8 @@ router.get('/searchMovies', (req, res) => {
       const movies = dbPostData.map(movie => movie.get({ plain: true }));
 
       res.render('searchMovies', {
-        movies
-        //loggedIn: req.session.loggedIn
+        movies,
+        loggedIn: req.session.loggedIn
       });
     })
     .catch(err => {
@@ -57,4 +59,27 @@ router.get('/searchMovies', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+
+router.get('/dashboard', (req, res) => {
+  Movies.findAll({
+    attributes: ['id', 'title', 'rating', 'description', 'status', 'user_id'],
+    where: {
+      user_id: req.session.user_id
+    },
+  })
+    .then(dbPostData => {
+      const movies = dbPostData.map(movie => movie.get({ plain: true }));
+      console.log(movies)
+      res.render('dashboard', {
+        movies,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
