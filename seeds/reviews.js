41241d298b0e1a8c58081e084abe181af854ac96
KevinGodek <92218951+KevinGodek@users.noreply.github.com>
2022-01-30 17:45:21 -0500

    added more reviews to seed file

--- seeds/reviews.js
+++ seeds/reviews.js
@@ -1,15 +1,50 @@
 const { Reviews } = require('../models');
 
-const review = [
+const postdata = [
   {
-		username: "User2",
+		user_id: "User2",
 		movie_id: "1",
 		rating: '5',
 		title: "Two thumbs up!",
 		description: "A must see movie."
 	},
+	{
+		user_id: "User3",
+		movie_id: "2",
+		rating: '5',
+		title: "Two thumbs down!",
+		description: "A must not see movie."	
+	},
+	{
+		user_id: "User3",
+		movie_id: "3",
+		rating: '2',
+		title: "Feel good movie of the season!",
+		description: "Such super good feelings."	
+	},
+	{
+		user_id: "User5",
+		movie_id: "4",
+		rating: '3',
+		title: "Super special movie.",
+		description: "A classic. #blessed"	
+	},
+	{
+		user_id: "User4",
+		movie_id: "5",
+		rating: '5',
+		title: "Not a total waste of money.",
+		description: "Such a magical movie."	
+	},
+	{
+		user_id: "User8",
+		movie_id: "6",
+		rating: '1',
+		title: "Stunning and brave performance",
+		description: "A rather shallow attemp at cinima."	
+	}
 ];
 
-const seedMovies = () => Post.bulkCreate(MoviesData);
+const seedReviews = () => Post.bulkCreate(ReviewsData);
 
-module.exports = seedMovies;
+module.exports = seedReviews;
