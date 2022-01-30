84fd2cff0d626e0b0d3efc41f46c19b17ddf8e1b
Erik Jones <jonzee1914@gmail.com>
2022-01-30 15:15:36 -0500

    added seeds index.js, and added to scripts

---
+++ seeds/index.js
@@ -0,0 +1,26 @@
+const seedUsers = require('./users');
+const seedMovies = require('./films');
+// const seedComments = require('./reviews');
+// const seedVotes = require('./rentals');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('--------------');
+  await seedUsers();
+  console.log('--------------');
+
+  await seedMovies();
+  console.log('--------------');
+
+  // await seedReviews();
+  // console.log('--------------');
+
+  // await seedRentals();
+  // console.log('--------------');
+
+  process.exit(0);
+};
+
+seedAll();
